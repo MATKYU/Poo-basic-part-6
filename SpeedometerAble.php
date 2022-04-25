56501e692755bf79baa3b5e830278ee7180b0311
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-04-25 16:57:56 +0200

    Add some files for the quest

---
+++ SpeedometerAble.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+class Speedometer
+{
+    const KM_TO_MILES = 0.621;
+    public static function convertKmToMiles(float $speed): float
+    {
+        return self::KM_TO_MILES*$speed;
+    }
+    public static function convertMilesToKm(float $speed): float
+    {
+        return $speed/self::KM_TO_MILES;
+    }
+}
+
+echo (Speedometer::convertKmToMiles(5));
